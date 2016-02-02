852316edbdd61e33aa884ad1f610fac1541b251b
Alexandre Aquiles <alexandre.aquiles@gmail.com>
2016-02-02 15:11:04 -0200

    removendo formatacao ruim de possivel h1 duplicado

--- theme/index.js
+++ theme/index.js
@@ -37,10 +37,7 @@ module.exports = {
 
 function obtainExtension(options) {
     var extension = options.extension || path.extname(options.output).replace(".", "");
-    if (!extension && options.format === "ebook") {
-        extension = "pdf";
-    }
-    return extension;
+    return extension || "pdf";
 }
 
 function setOptions(){
